b89ddd07011c0d958d4781ffbb6184aaa93b86ab
SHV2110 <49692046+SHV2110@users.noreply.github.com>
2021-11-26 03:57:15 +0000

    Commit

--- SpringBootWebPayment/.mvn/src/main/java/com/payment/spring/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payment.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.payment.spring.model.Bank;
-
-public interface BankRepository extends JpaRepository<Bank,String>{
-
-}
--- SpringBootWebPayment/src/main/java/com/payment/spring/repository/TransfertypesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payment.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.payment.spring.model.Transfertypes;
-
-public interface TransfertypesRepository extends JpaRepository<Transfertypes,String> {
-
-}
